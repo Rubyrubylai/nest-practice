31c8d4559f782804498af74b7aac1603e7b9c4a6
Rubyrubylai <r844312@gmail.com>
2023-05-06 16:53:43 +0800

    practice middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { AnotherCatsModule } from './another-cats/another-cats.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
-@Module({
-  imports: [],
+@Module({  // provides metadata that Nest makes use of to organize the application structure.
+  imports: [CatsModule, AnotherCatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET })
+  }
+}
